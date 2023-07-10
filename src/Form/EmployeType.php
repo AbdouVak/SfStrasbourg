2eea648faaff860eec3433c89e208e900b2dd99d
AbdouVak <abdou.vakouiev@gmail.com>
2023-07-10 11:56:19 +0200

    EmployeType: modification eshetique et des type pour chaque add

--- src/Form/EmployeType.php
+++ src/Form/EmployeType.php
@@ -3,38 +3,49 @@
 namespace App\Form;
 
 use App\Entity\Employe;
+use App\Entity\Entreprise;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EmployeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', [
-                'attr' => ['class' => 'btn btn-success']
-            ])
-            
-            ->add('prenom', [
-                'attr' => ['class' => 'btn btn-success']
+            ->add('nom', TextType::class, [
+                'attr' => ['class' => 'form-control']
             ])
 
-            ->add('dateNaissance', [
+            ->add('prenom', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+
+            ->add('dateNaissance', DateType::class, [
                 'widget' =>'single_text',
                 'attr' => ['class' => 'form-control']
             ])
 
-            ->add('dateEmbauche', [
+            ->add('dateEmbauche', DateType::class, [
                 'widget' =>'single_text',
                 'attr' => ['class' => 'form-control']
             ])
 
-            ->add('ville', [
-                'attr' => ['class' => 'btn btn-success']
+            ->add('ville', TextType::class, [
+                'attr' => ['class' => 'form-control']
             ])
 
-            ->add('entreprise', [
+            ->add('entreprise', EntityType::class, [
+                'class' => Entreprise::class,
+                'choice_label' => 'raisonSociale',
+                'attr' => ['class' => 'form-control']
+            ])
+
+            ->add('submit',SubmitType::class, [
                 'attr' => ['class' => 'btn btn-success']
             ])
         ;
